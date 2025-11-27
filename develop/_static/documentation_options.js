240b49b7bc8eea787de4bf3326e571f1d26fb7f2
bwentl <bwentl@users.noreply.github.com>
2025-11-27 19:26:35 +0000

    deploy: 5863b27f9644be31f354fc882652a93cf03127aa

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1000.dev3482+g5e781a705',
+    VERSION: '1000.dev3493+g5863b27f9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
